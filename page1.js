9512c72d611d0e0a10766cf110c49c321422bbb7
jo777750 <66462046+jo777750@users.noreply.github.com>
2022-09-28 12:23:14 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,22 @@
+
+      console.log("JOEEU");
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "./test.html", true);
+xhr.onload = (e) => {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(`hi:${xhr.responseText}`);
+    } else {
+      console.error(xhr.statusText);
+    }
+  }
+};
+xhr.onerror = (e) => {
+  console.error(xhr.statusText);
+};
+xhr.send(null);
+ document.getElementById('test_html').addEventListener('click', () => {
+          	  window.open("./test.html","JS Calci");
+        
+           });
+					
