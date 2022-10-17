25247ba8e42f1deba438ff4947c6ed90d42a86f4
jo777750 <66462046+jo777750@users.noreply.github.com>
2022-10-17 23:55:56 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     (async () => console.log(await b()))(),
     (async () => console.log(await c()))(),
     (async () => console.log(await d()))(),
+    (async () => console.log(await e()))(),
 
   ]);
 }
@@ -130,6 +131,29 @@ document.getElementById('firstrow').appendChild(a1);
 return Promise.resolve(3);
 }
 //--------------------------------------------------
+ 
+function e(){
+ const myRequest = new Request("a.json");
+  console.log(myRequest);
+fetch(myRequest).then (async response => {
+cc=await response.json() 
+//console.log(typeof cc)
+for (var keykk in cc){
+a1 = document.createElement('a');
+a1.setAttribute('title', cc[keykk]);
+a1.setAttribute('href', keykk);
+a1.innerHTML=a1.title+"<br>";//perfect place using to put a new line
+document.getElementById('json').appendChild(a1);
+}
+for (const property in cc) {
+  console.log(`${property}: ${cc[property]}`);}
+});
+fetch('https://www.cnn.com')
+ return Promise.resolve(6);
+}
+//--------------------------------------------------
+ 
+ 
 function c(){
 let json2 = '{"https://developers.google.com/edu/python/regular-expressions":"Python regular expressions",\
 "https://learnbyexample.github.io/py_regular_expressions/anchors.html":"Python regular",\
