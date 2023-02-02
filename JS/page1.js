a3d2e34f7fb8cd0b774194559785d93f22f931d4
jo777750 <66462046+jo777750@users.noreply.github.com>
2023-02-02 23:24:33 +0530

    Create page1.js

---
+++ JS/page1.js
@@ -0,0 +1,27 @@
+
+console.log("JOEEU");
+/* const xhr = new XMLHttpRequest();
+xhr.open("GET", "./test.html", true);
+xhr.onload = (e) => {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(`hi:${xhr.responseText}`);
+	   document.getElementById('test_html').addEventListener('click', () => {
+          	  window.open("./test.html","JS Calci");
+        
+           });
+    } else {
+      console.error(xhr.statusText);
+    }
+  }
+};
+xhr.onerror = (e) => {
+  console.error(xhr.statusText);
+};
+xhr.send(null);
+ */
+					
+document.getElementById('test_html').addEventListener('click', () => {
+          	  window.open("../test.html","JS Calci");
+        
+           });
